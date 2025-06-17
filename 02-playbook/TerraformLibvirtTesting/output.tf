685180ea1b1afbe05208c660df711ccfd1619384
Spardoksis <vitasman122@gmail.com>
2025-06-17 21:53:37 +0300

    Task #3: create Lighthouse-role to use in ansible reqs

--- 02-playbook/TerraformLibvirtTesting/output.tf
+++
@@ -1,9 +0,0 @@
-output "vm_name" {
-  value       = libvirt_domain.vm.name
-  description = "VM name"
-}
-
-output "vm_ip" {
-  value       = libvirt_domain.vm.network_interface[0].addresses.0
-  description = "Interface IPs"
-}
--- 02-playbook/TerraformLibvirtTesting/providers.tf
+++
@@ -1,3 +0,0 @@
-provider "libvirt" {
-    uri = "qemu+ssh://tester@127.0.0.1/system"
-}
